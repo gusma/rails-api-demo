3aa39a400d9b5a887705a75235f9db862da188ae
Gustavo Malamud <gustavo.malamud@gmail.com>
2022-05-27 01:27:32 -0300

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :v1 do
-      resources :locations do
-        resources :recordings
-      end
-    end
-  end
-
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :locations
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-l = Location.create(name: "Buenos Aires")
-l.recordings.create(temp: 32, status: "Nublado")
-l.recordings.create(temp: 34, status: "Nublado")
-l.recordings.create(temp: 33, status: "Nublado")
-l.recordings.create(temp: 32, status: "Lluvioso")
-l.recordings.create(temp: 32, status: "Nublado")
-l.recordings.create(temp: 28, status: "Lluvioso")
